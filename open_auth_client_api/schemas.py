71ea70e193d29a7402e0e49c0ad29c09288e7f5a
KJ <19541224+reddpy@users.noreply.github.com>
2023-01-16 00:20:21 -0500

    more schema validators and duplicate user checks

--- open_auth_client_api/schemas.py
+++ open_auth_client_api/schemas.py
@@ -16,7 +16,12 @@ class UserBase(BaseModel):
         return v
 
     @validator("phone")
+    def strip_spaces(cls, v):
+        return v.strip().replace(" ", "")
+
+    @validator("phone")
     def valid_number_regex(cls, v):
+        print(v)
         regex_expression = "^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$"
 
         match_obj = re.match(regex_expression, v)
@@ -24,6 +29,19 @@ class UserBase(BaseModel):
             raise ValueError("phone is not valid")
 
         return v
+
+    @validator("phone")
+    def ph_max_length(cls, v):
+        wanted_chars = list("1234567890")
+
+        valid_chars = [i for i in v if i in wanted_chars]
+        print(valid_chars)
+
+        max_length = 16
+        if len(v) > max_length:
+            raise ValueError("phone is not valid length")
+
+        return "".join(valid_chars)
 
 
 class UserCreate(UserBase):
